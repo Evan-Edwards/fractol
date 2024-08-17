a34f463d57824b7f8b2f0879d9521d7550afef9e
evan <evanjonedwards@gmail.com>
2024-08-17 13:26:10 +0300

    some progress

--- f_main.c
+++ f_main.c
@@ -6,24 +6,27 @@
 /*   By: eedwards <eedwards@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 14:45:38 by eedwards          #+#    #+#             */
-/*   Updated: 2024/08/14 12:44:42 by eedwards         ###   ########.fr       */
+/*   Updated: 2024/08/17 13:25:44 by eedwards         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-
-int	ft_close(t_vars *vars)
+//destroys display and frees data used
+int	ft_close(t_fractal *vars)
 {
 	mlx_destroy_display(vars->mlx);
-	free (vars->mlx); //neccessary to free?
+	free (vars->mlx);
 	exit (0);
 }
 
 int	key_hook(int keycode, t_vars *vars) //does it need to be a t_vars *?
 {
+	int	esc;
+
+	esc = 65307;
 	ft_printf("keycode is: %i", keycode);
-	if (keycode == 65307)
+	if (keycode == esc)
 		ft_close(vars);
 	return (0);
 }
@@ -36,66 +39,148 @@ void	pixel_put_image(t_img *img, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
+void	ft_set_compl_values(double real, double imaginary, t_complex *cz)
+{
+	cz->real = real;
+	cz->imaginary = imaginary;
+	cz->absolute_2 = real * real + imaginary * imaginary;
+}
+
+int	ft_mandelbrot_check(int x, int y)
+{
+	t_complex	c;
+	t_complex	z;
+	double		z_real;
+	double		z_imag;
+	int			iteration;
+
+	ft_set_compl_values(x, y, &c);
+	ft_set_compl_values(0, 0, &z);
+	//need to scale, should go from -2 to 2 for each axis. 
+	iteration = 0;
+	while (iteration < 1000 && z.absolute_2 <= 4)
+	{
+		z_real = z.real * z_real - z.imaginary * z.imaginary + c.real;
+		z_imag = 2 * z.real * z.imaginary + c.imaginary;
+		ft_set_compl_values(z_real, z_imag, &z);
+		iteration++;
+	}
+	return (iteration == 1000);
+}
+int	ft_julia_check(int x, int y, char **av)
+{
+	t_complex	c;
+	t_complex	z;
+	int		z_real;
+	int		z_imag;
+	int	iteration;
+
+	ft_set_compl_values(ft_atoi(av[2]), ft_atoi(av[3]), &c);
+	ft_set_compl_values(x, y, &z);
+	iteration = 0;
+	while (iteration < 1000 && z.absolute_2 <= 4)
+	{
+		z_real = z.real * z_real - z.imaginary * z.imaginary + c.real;
+		z_imag = 2 * z.real * z.imaginary + c.imaginary;
+		ft_set_compl_values(z_real, z_imag, &z);
+		iteration++;
+	}
+	return (iteration == 1000);
+}
+
+
 void	parse_pixels(t_img *img, char **av)
 {
 	int		x;
 	int		y;
 	int 	color;
-	char	*temp;
+	int		color2;
 	
-	//go through every pixel, go through all the x values in a line
-	//then increase y, x starts at 0 until it reaches the last pixel
+	//go through ever x and y and 
 	//then push the image
 	//have to also make it into a grid
 	//start over?
-	temp = img->addr;
-	while (img->addr != NULL)
+	
+	color = 6561336;
+	color2 = 15426616;
+	x = 0; //what about if max_x is even, should i put into ft_set_max_xy?
+	y = 0; //same as above
+	while (y < img->max_y)
 	{
-		y = (temp - img->addr) / img->line_length;
-		x = (temp - img->addr) % img->line_length / (img->bbp / 8);
-		if (ft_strncmp(av[1], "mandelbrot", 10))
+		while (x < img->max_x)
 		{
-			if (ft_mandelbrot_check(x, y) == 1)
-				pixel_put_image(img, x, y, color) ;// change color
-			else
-				pixel_put_image(img, x, y, color);// different color
+			if (!(ft_strncmp(av[1], "mandelbrot", 10)))
+			{
+				if (ft_mandelbrot_check(x - img->max_x / 2, img->max_y / 2 - y) == 1)
+				pixel_put_image(img, x, y, color);
+				else
+				pixel_put_image(img, x, y, color2);
+			}
+			else 
+			{
+				if (ft_julia_check(x - img->max_x / 2, img->max_y / 2 - y, av) == 1)
+					pixel_put_image(img, x, y, color);
+				else
+					pixel_put_image(img, x, y, color2);
+			}
+			x++;
 		}
-		else 
-		{
-			if (ft_julia_check(x, y, av) == 1)
-				pixel_put_image(img, x, y, color); // change color
-			else
-				pixel_put_image(img, x, y, color);// different color
-		}
+		x = 0;
+		y++; 
 	}
 }
 
+void	ft_set_max_xy(t_img *img)
+{
+	char	*last_address;
+
+	img->max_x = img->line_length / (img->bbp / 8);
+	last_address = img->addr;
+	while (last_address != NULL)
+		last_address++;
+	img->max_y = (last_address - img->addr) / img->line_length + 1;
+}
+
+void	fractal_init(t_fractal *fractal)
+{
+	fractal->mlx = mlx_init();
+	if (!fractal->mlx)
+		malloc_error(); //TODO
+	fractal->win = mlx_new_window(fractal->mlx, WIDTH, HEIGHT, fractal->title);
+	if (!fractal->win)
+		ft_close(fractal);
+	fractal->img = mlx_new_image(fractal->mlx, WIDTH, HEIGHT);
+	//check
+	fractal->img_addr = mlx_get_data_addr(fractal->img, &fractal->bbp, 
+										&fractal->line_length, &fractal->endian);
+	//events_init; //TODO
+	//data_init//TODO
+
+}
 
 int	main(int ac, char **av)
 {
-	t_vars	vars;
-	t_img	img;
+	t_fractal	fractal;
 
 	if ((ac == 2 && !ft_strncmp(av[1], "mandelbrot", 10))
 		|| (ac == 4 && !ft_strncmp(av[1], "julia", 5)))
-		ft_putstr("Input valid");
-		//do program
+		{
+			ft_putstr("Input valid");
+			fractal.title = av[1];
+			
+		}
+		
 	else
 	{
-		ft_putstr_fd("Please provide valid parameter, for example: ", 1); //TODO
+		ft_putstr_fd("Input invalid. Valid inputs: mandelbot OR julia 1 3\n", 1);
+		return (0); //TODO
 	} 
-	vars.mlx = mlx_init();
-	if (!vars.mlx)
-		return (1);
-	//graphical stuff
 	//clean up display struct and xvar struct
-	vars.win = mlx_new_window(vars.mlx, 1920, 1080, "Hello World!");
-	if (!vars.win)
-		return (1);
 	mlx_key_hook(vars.win, key_hook, &vars);
 	mlx_hook(vars.win, 17, 0, ft_close, &vars);
 	img.img = mlx_new_image(vars.mlx, 1920, 1080);
 	img.addr = mlx_get_data_addr(img.img, &img.bbp, &img.line_length, &img.endian);
+	ft_set_max_xy(&img);
 	parse_pixels(&img, av);
 	mlx_pixel_put(vars.mlx, vars.win, 100, 100, 16777215);
 	mlx_loop(vars.mlx);
--- fractol.h
+++ fractol.h
@@ -6,31 +6,55 @@
 /*   By: eedwards <eedwards@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 14:47:27 by eedwards          #+#    #+#             */
-/*   Updated: 2024/08/13 15:35:08 by eedwards         ###   ########.fr       */
+/*   Updated: 2024/08/17 13:25:47 by eedwards         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTAL
 #define FRACTAL
 
-#include "minilibx/mlx.h"
+#include "minilibx/mlx.h" //doesn't work at home
 #include "libft/libft.h"
 #include <stdlib.h>
 #include <math.h>
+#include <stdlib.h> //malloc free
+#include <unistd.h> //write
+
+#define WIDTH	800
+#define HEIGHT	800
 
 typedef struct	s_vars {
-	void	*mlx;
-	void	*win;
+	
 }				t_vars;
 
-typedef struct	s_img {
+typedef struct	s_fractal {
+	char	*title;
+	void	*mlx;
+	void	*win;
 	void	*img;
-	char	*addr;
+	char	*img_addr;
 	int		bbp;
 	int		line_length; //number of bytes used to store single row of pixels
 	int		endian; //determines how pixels stored in memory (least or most signifcant first)
-}				t_img;
+	int		max_x;
+	int		max_y;
+}				t_fractal;
 
+typedef struct	s_complex
+{
+	double	real;
+	double	imaginary;
+	double	absolute_2;
+}				t_complex;
+
+int		ft_close(t_vars *vars);
+int		ft_julia_check(int x, int y, char **av);
+int		ft_mandelbrot_check(int x, int y);
+void	ft_set_compl_values(int real, int imaginary, t_complex *cz);
+void	ft_set_max_xy(t_img *img);
+int		key_hook(int keycode, t_vars *vars);
+void	pixel_put_image(t_img *img, int x, int y, int color);
+void	parse_pixels(t_img *img, char **av);
 
 
 #endif
